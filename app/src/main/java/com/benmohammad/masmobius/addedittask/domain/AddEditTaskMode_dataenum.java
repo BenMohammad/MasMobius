0ec516823b0e0ff430259872bc6b1bc0632fb2da
Ben Mohammad <benmohammad593@gmail.com>
2020-03-05 16:06:15 +0000

    datenum added...(AddEditTaskMode)

---
+++ app/src/main/java/com/benmohammad/masmobius/addedittask/domain/AddEditTaskMode_dataenum.java
@@ -0,0 +1,11 @@
+package com.benmohammad.masmobius.addedittask.domain;
+
+import com.spotify.dataenum.DataEnum;
+import com.spotify.dataenum.dataenum_case;
+
+@DataEnum
+public interface AddEditTaskMode_dataenum {
+
+    dataenum_case Create();
+    dataenum_case Update(String id);
+}
