a93077e89aee36c0abf16847eb8452fbc89428d4
Ben Mohammad <benmohammad593@gmail.com>
2020-03-05 16:12:44 +0000

    autoValue class added...

---
+++ app/src/main/java/com/benmohammad/masmobius/addedittask/domain/AddEditTaskModel.java
@@ -0,0 +1,28 @@
+package com.benmohammad.masmobius.addedittask.domain;
+
+import com.benmohammad.masmobius.data.TaskDetails;
+import com.google.auto.value.AutoValue;
+
+@AutoValue
+public abstract class AddEditTaskModel {
+
+    public abstract AddEditTaskMode mode();
+    public abstract TaskDetails details();
+
+    public AddEditTaskModel withDetails(TaskDetails details) {
+        return toBuilder().details(details).build();
+    }
+
+    public abstract Builder toBuilder();
+
+    public static Builder builder() {
+        return new AutoValue_AddEditTaskModel.Builder();
+    }
+
+    @AutoValue.Builder
+    public abstract static class  Builder {
+        public abstract Builder mode(AddEditTaskMode mode);
+        public abstract Builder details(TaskDetails details);
+        public abstract AddEditTaskModel build();
+    }
+}
