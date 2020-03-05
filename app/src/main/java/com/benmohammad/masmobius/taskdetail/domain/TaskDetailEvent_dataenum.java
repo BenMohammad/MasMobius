343d91fdae67d5870993912ed4169f6d9c38be27
Ben Mohammad <benmohammad593@gmail.com>
2020-03-05 12:27:48 +0000

    TaskDetailEvent_dataenum added....

---
+++ app/src/main/java/com/benmohammad/masmobius/taskdetail/domain/TaskDetailEvent_dataenum.java
@@ -0,0 +1,18 @@
+package com.benmohammad.masmobius.taskdetail.domain;
+
+import com.spotify.dataenum.DataEnum;
+import com.spotify.dataenum.dataenum_case;
+
+@DataEnum
+public interface TaskDetailEvent_dataenum {
+
+    dataenum_case DeleteTaskRequested();
+    dataenum_case CompleteTaskRequested();
+    dataenum_case ActivateTaskRequested();
+    dataenum_case EditTaskRequested();
+    dataenum_case TaskDeleted();
+    dataenum_case TaskMarkedComplete();
+    dataenum_case TaskMarkedActive();
+    dataenum_case TaskSaveFailed();
+    dataenum_case TaskDeletionFailed();
+}
